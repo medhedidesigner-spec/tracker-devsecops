9cb1ca708fcc966e6128d35f1bb2db27141a9afe
elhedihaddar <medhedi.designer@gmail.com>
2026-06-29 13:26:41 +0200

    fix: base path pour GitHub Pages rollback

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT : remplace 'tracker-devsecops' par le nom exact de ton repo GitHub
-  base: '/tracker-v2/',
+  base: '/tracker-devsecops/',
 })
